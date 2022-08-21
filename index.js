c702999b1b039a64bddec09a64acc399e00d9fba
David Lambert <david.lambert@my.utsa.edu>
2022-08-21 12:13:00 -0500

    added 64 bit encryption and card class input

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault()
 
-    const input = document.querySelector('input')
-    console.log(input.value)
+    document.querySelector('#message-form').classList.add("hide")
+    document.querySelector('#link-form').classList.remove("hide")
+
+    const input = document.querySelector('#message-input')
+        //btoa built in method to go from string to base 64, atob converts it back
+    const encrypted = btoa(input.value)
+
+    const inputLink = document.querySelector('#link-input')
+    inputLink.value = `${window.location}#${encrypted}`
+        //selects input and text inside, for copying
+    inputLink.select()
 })
