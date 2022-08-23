70cfffee836034775c5f4740e24a90647f84101e
David Lambert <david.lambert@my.utsa.edu>
2022-08-23 15:47:03 -0500

    formatted code

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const { hash } = window.location
+const { hash } = window.location;
 
-const message = atob(hash.replace('#', ""))
+const message = atob(hash.replace("#", ""));
 
 if (message) {
-    document.querySelector('#message-form').classList.add('hide')
-    document.querySelector('#message-show').classList.remove('hide')
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#message-show").classList.remove("hide");
 
-    document.querySelector('h1').innerHTML = message
+  document.querySelector("h1").innerHTML = message;
 }
 
-document.querySelector('form').addEventListener('submit', event => {
-    event.preventDefault()
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    document.querySelector('#message-form').classList.add("hide")
-    document.querySelector('#link-form').classList.remove("hide")
+  document.querySelector("#message-form").classList.add("hide");
+  document.querySelector("#link-form").classList.remove("hide");
 
-    const input = document.querySelector('#message-input')
-        //btoa built in method to go from string to base 64, atob converts it back
-    const encrypted = btoa(input.value)
+  const input = document.querySelector("#message-input");
+  //btoa built in method to go from string to base 64, atob converts it back
+  const encrypted = btoa(input.value);
 
-    const inputLink = document.querySelector('#link-input')
-    inputLink.value = `${window.location}#${encrypted}`
-        //selects input and text inside, for copying
-    inputLink.select()
-})
+  const inputLink = document.querySelector("#link-input");
+  inputLink.value = `${window.location}#${encrypted}`;
+  //selects input and text inside, for copying
+  inputLink.select();
+});
